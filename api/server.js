ab7fd29dcbaefd43c3c620f1de5dbf403b5b0439
Roger Enand <rogerenand@gmail.com>
2021-01-22 09:06:58 -0800

    added api server

---
+++ api/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+import Express from "express";
+var cors = require('cors');
+var mysql = require('mysql');
+const bodyParser = require('body-parser');
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DB
+});
+
+const app = Express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const port = 9000;
+connection.connect();
+
+app.get("/test", (req, res) => {
+  res.send('Connected To Back End');
+});
+
+app.get("/babynames", (req, res) => {
+  connection.query('Select * from baby_names', function (err, rows, fields) {
+    if (err) throw err
+    var returnedRows = rows;
+    res.send(returnedRows);
+  })
+});
+
+app.post("/babynames", (req, res) => {
+  connection.query("INSERT INTO `expanding_family`.`baby_names` (`suggestors_name`, `baby_name_suggestion`, `baby_gender`) VALUES ('" + req.body.suggestors_name + "', '" + req.body.baby_name_suggestion + "', '" + req.body.baby_gender + "')", function (err, rows, fields) {
+    if (err) throw err
+    // var returnedRows = rows;
+    // res.send(returnedRows);
+    res.send('Success');
+  })
+})
+
+app.listen(port, () => console.log("listening on port: " + port))
