92c1d0774f11188cdbdaa6a7c4bc0d1bd666775d
Roger Enand <rogerenand@gmail.com>
2021-01-23 00:03:14 -0800

    chaged to /active

--- src/app/Services/active.service.ts
+++ src/app/Services/active.service.ts
@@ -21,18 +21,18 @@ export class ActiveService {
     let toReturn = null;
     const postBody = JSON.stringify({
         "id": 1,
-        "name_suggestion": name,
-        "gender_reveal": gender,
-        "progress": progress,
-        "announcments": announcements,
-        "events": events
+        "name_suggestion": name + '',
+        "gender_reveal": gender + '',
+        "progress": progress + '',
+        "announcments": announcements + '',
+        "events": events + ''
     })
     const options = {
       headers: { "Content-type": "application/json; charset=UTF-8" },
       method: 'put',
       body: postBody
     }
-    await fetch('http://3.134.168.146:9000/babynames', options)
+    await fetch('http://3.134.168.146:9000/active', options)
       // .then(res => res.json())
       .then(res => {
         toReturn = res;
