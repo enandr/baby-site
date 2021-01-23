63af3258c2aa356264729380850a0746bd748b3d
Roger Enand <rogerenand@gmail.com>
2021-01-23 00:52:01 -0800

    removed reg

--- api/server.js
+++ api/server.js
@@ -82,9 +82,9 @@ app.get("/active", (req, res) => {
 });
 
 app.put("/active", (req, res) => {
-  connection.query("UPDATE `expanding_family`.`active_items` SET `name_suggestion` = '" + req.body.name_suggestion + "', `gender_reveal` = '" + req.body.gender_reveal + "', `progress` = '" + req.body.progress + "', `announcments` = '" + req.body.announcments + "', `events` = '" + req.body.events + "', `registry` = '" + req.body.registry + "' WHERE (`id` = '1')", function (err, rows, fields) {
+  connection.query("UPDATE `expanding_family`.`active_items` SET `name_suggestion` = '" + req.body.name_suggestion + "', `gender_reveal` = '" + req.body.gender_reveal + "', `progress` = '" + req.body.progress + "', `announcments` = '" + req.body.announcments + "', `events` = '" + req.body.events + "' WHERE (`id` = '1')", function (err, rows, fields) {
     if (err) throw err
-    // ', `registry_url` = '" + req.body.registry_url + "
+    // ', `registry` = '" + req.body.registry + "', `registry_url` = '" + req.body.registry_url + "
     // var returnedRows = rows;
     // res.send(returnedRows);
     res.send('Success');
