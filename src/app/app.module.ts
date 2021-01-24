ec9dbfae1d63ab032196f671c998aacd87aefc9a
Roger Enand <rogerenand@gmail.com>
2021-01-23 22:03:46 -0800

    changed to uploads

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FileUploadModule } from 'ng2-file-upload';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { AdminComponent } from './Pages/admin/admin.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FileUploadModule
+    FileUploadModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
