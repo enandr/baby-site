ac476125f74f8f1a75193ec41d961cab12c8f68c
Roger Enand <rogerenand@gmail.com>
2021-01-23 22:43:20 -0800

    removed filename

--- api/server.js
+++ api/server.js
@@ -111,16 +111,15 @@ app.get("/photos", (req, res) => {
 app.post('/photos', upload.single('file'), function (req, res) {
   const file = req.file;
   if (file) {
-    var filename = file.originalname;
     console.log(file.originalname);
 
-    /* fs.move('./uploads' + fileName, '../tempDir/' + 'testfolder' + '/' + fileName, function (err) {
+    fs.move('./uploads' + file.originalname, '../tempDir/' + 'testfolder' + '/' + file.originalname, function (err) {
       if (err) {
         return console.error(err);
       }
 
       res.json({});
-    }); */
+    });
     res.send('file');
   } else {
     throw new Error('file error');
