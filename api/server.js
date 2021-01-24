fbf60b772c6613056f64fc5f1a6c285fa9e5473a
Roger Enand <rogerenand@gmail.com>
2021-01-23 22:45:35 -0800

    changes

--- api/server.js
+++ api/server.js
@@ -113,7 +113,7 @@ app.post('/photos', upload.single('file'), function (req, res) {
   if (file) {
     console.log(file.originalname);
 
-    fs.move('./uploads/' + file.originalname, '../tempDir/' + 'testfolder' + '/' + file.originalname, function (err) {
+    fs.move('./uploads/' + file.originalname, './tempDir/' + 'testfolder' + '/' + file.originalname, function (err) {
       if (err) {
         return console.error(err);
       }
