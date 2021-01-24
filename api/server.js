2b8f843628c9289a454ca4521bc776aadf26efac
Roger Enand <rogerenand@gmail.com>
2021-01-24 00:30:27 -0800

    added url

--- api/server.js
+++ api/server.js
@@ -135,7 +135,7 @@ app.delete("/photos", (req, res) => {
   let path = '';
   connection.query("SELECT * FROM `expanding_family`.`photos` WHERE (`id` = '" + req.body.id + "')", function (err, rows, fields) {
     if (err) throw err
-    path = rows[0].url;
+    path = '../src/dist/baby-site' + rows[0].url;
     console.log(path);
   })
   /* connection.query("DELETE FROM `expanding_family`.`photos` WHERE (`id` = '" + req.body.id + "')", function (err, rows, fields) {
